272373c382cd78115a926796216c34ed1eeedf75
Chi Ho Lee <clee@intelycare.com>
2024-01-28 19:29:11 -0500

    Thread.sleep(60000) // 60 second

--- app/src/androidTest/java/com/example/githubactionapplication/HelloWorldTest.kt
+++ app/src/androidTest/java/com/example/githubactionapplication/HelloWorldTest.kt
@@ -21,7 +21,7 @@ class HelloWorldTest {
     fun helloWorld() {
         val intent = Intent(InstrumentationRegistry.getInstrumentation().targetContext, HelloActivity::class.java)
         scenario = ActivityScenario.launch(intent)
-        Thread.sleep(10000) // 10 second
+        Thread.sleep(60000) // 60 second
         onView(withText("Hello Android!")).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         scenario.close()
     }
